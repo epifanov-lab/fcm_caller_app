e2a505ad9209909b0eac4c03b313206e4dcdc900
Konstantin Epifanov <epifanov.konstantin.lab@gmail.com>
2020-04-10 10:53:20 +0300

    WssService: channels, data receive/send, echo test

--- lib/services/wss_service.dart
+++ lib/services/wss_service.dart
@@ -9,49 +9,88 @@ class WssService {
 
   final PublishSubject<AppLifecycleState> _lifecycleEvents;
   final Stream<ConnectivityResult> _connectivityEvents;
-
   Observable<bool> _connectability;
-  IOWebSocketChannel _channel;
 
-  final Subject<Map<String, dynamic>> changes = BehaviorSubject();
+  Subject<IOWebSocketChannel> _channels;
+  IOWebSocketChannel _currentChannel;
+
+  StreamController<String> _data;
+
+  Subject<Map<String, dynamic>> states;
   final Map<String, dynamic> _info = {
     'lifecycle': AppLifecycleState.detached,
     'connectivity': ConnectivityResult.none,
     'connectability': false,
+    'channel.hashCode': null,
+    'channels_closed': 0
   };
 
   WssService()
       : _lifecycleEvents = PublishSubject(),
         _connectivityEvents = Connectivity().onConnectivityChanged {
-    changes.add(_info);
+    _currentChannel = _createWsChannel();
+    _channels = BehaviorSubject.seeded(_currentChannel);
+    _data = StreamController.broadcast();
+    states = BehaviorSubject.seeded(_info);
 
     _connectability = Observable.combineLatest([
       _lifecycleEvents.debounceTime(new Duration(milliseconds: 500))
-          .doOnEach((n) => updateStateMap('lifecycle', n.value))
+          .doOnEach((n) => _updateStateMap('lifecycle', n.value))
           .map((state) => state == AppLifecycleState.resumed),
       Observable(_connectivityEvents).debounceTime(new Duration(milliseconds: 1000))
-          .doOnEach((n) => updateStateMap('connectivity', n.value))
+          .doOnEach((n) => _updateStateMap('connectivity', n.value))
           .map((state) => (state == ConnectivityResult.mobile || state == ConnectivityResult.wifi))
     ], (states) => _concatBooleans(states));
 
-       _connectability.listen((state) => _onConnectabilityChanges(state));
+       _connectability.listen((state) {
+         _updateStateMap('connectability', state);
+         if (state) {
+           if (_currentChannel.closeCode != null)
+             _channels.add(_createWsChannel());
+         } else {
+           if (_currentChannel.closeCode == null)
+             _currentChannel.sink.close();
+         }
+       });
+
+       _channels.distinct().listen((channel) {
+         _updateStateMap('channel.hashCode', channel.hashCode);
+         _currentChannel = channel
+           ..stream.listen((message) => _receiveMessage(message),
+               onDone: () => _updateStateMap('channels_closed', ++_info['channels_closed']));
+       });
   }
 
-  void _onConnectabilityChanges(bool state) {
-    updateStateMap('connectability', state);
-    //todo channel
+  Observable events() => Observable(_data.stream);
+
+  void _receiveMessage(message) {
+    print('ws <<< $message');
+    _data.add(message);
   }
 
-  void updateStateMap(String key, dynamic value) {
+  Future sendMessage(String message) {
+    print('ws >>> $message');
+    if (_currentChannel.closeCode == null) {
+      _currentChannel.sink.add(message);
+      return Future.value(null);
+    } else return Future.error('Send message to ws failed. channel is closed.');
+  }
+
+  IOWebSocketChannel _createWsChannel() => IOWebSocketChannel.connect('ws://echo.websocket.org');
+
+  void _updateStateMap(String key, dynamic value) {
     print('wss: state changes: $key = $value');
     _info[key] = value;
-    changes.add(_info);
+    states.add(_info);
   }
 
   void onLifecycleStateChanged(AppLifecycleState state) => _lifecycleEvents.add(state);
+
   void dispose() {
     _lifecycleEvents.close();
-    changes.close();
+    _channels.close();
+    states.close();
+    _data.close();
   }
 
   static bool _concatBooleans(Iterable booleans) {
--- test/test_wss_service.dart
+++ test/test_wss_service.dart
@@ -1,23 +1,44 @@
 import 'package:fcmcallerapp/theme.dart';
 import 'package:flutter/material.dart';
+import 'package:rxdart/rxdart.dart';
 
 import 'test_main.dart';
 
-class TestWssService extends StatelessWidget {
+class TestWssService extends StatefulWidget {
   static final String title = 'wss service';
+
+  @override
+  _TestWssServiceState createState() => _TestWssServiceState();
+}
+
+class _TestWssServiceState extends State<TestWssService> {
+
+  @override
+  void initState() {
+    runWssTest();
+    super.initState();
+  }
+
+  void runWssTest() {
+    Observable.range(0, 20) //todo не то
+        .delay(Duration(seconds: 2))
+        .listen((number) => wss.sendMessage('message $number'));
+
+    wss.events().listen((event) {});
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: Text('test-zone: $title')),
+        appBar: AppBar(title: Text('test-zone: ${TestWssService.title}')),
         body: SafeArea(
           child: Padding(
             padding: const EdgeInsets.all(16.0),
             child: StreamBuilder(
-              stream: wss.changes,
+              stream: wss.states,
               builder: (BuildContext context, AsyncSnapshot<Map<String, dynamic>> snapshot) {
                 if (snapshot.hasError) return Text('Error: ${snapshot.error}');
-                else if (!(snapshot.data is Map)) return Container();
+                else if (!snapshot.hasData) return Container();
                 else return ListView.builder(
                     itemCount: snapshot.data.keys.length,
                     itemBuilder: (context, index) =>
@@ -31,5 +52,4 @@ class TestWssService extends StatelessWidget {
         )
     );
   }
-
 }
